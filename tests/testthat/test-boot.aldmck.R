6a2b71369ac7e4cefa708fc2c666b586966482b0
David Liao <davidycliao@gmail.com>
2024-08-09 02:18:14 +0100

    UPDATE

--- tests/testthat/test-boot.aldmck.R
+++ tests/testthat/test-boot.aldmck.R
@@ -7,23 +7,23 @@ library(basicspace)
 
 test_that("boot.aldmck generates correct output structure", {
   data(franceEES2009)
-  
+
   # Run the bootstrap analysis
   boot_result <- boot.aldmck(franceEES2009, polarity = 2, respondent = 1, missing = c(77, 88, 89), verbose = FALSE, boot.args = list(R = 100))
-  
+
   # Check if the result is a list
   expect_type(boot_result, "list")
-  
+
   # Check if the list has the correct components
   expect_true("sumstats" %in% names(boot_result))
   expect_true("bootres" %in% names(boot_result))
-  
+
   # Check if sumstats is a dataframe
   expect_s3_class(boot_result$sumstats, "data.frame")
-  
+
   # Check if bootres is of class "boot"
   expect_s3_class(boot_result$bootres, "boot")
-  
+
   # Check the structure of sumstats
   sumstats <- boot_result$sumstats
   expect_true(all(c("stimulus", "idealpt", "sd", "lower", "upper") %in% names(sumstats)))
@@ -32,7 +32,7 @@ test_that("boot.aldmck generates correct output structure", {
   expect_type(sumstats$sd, "double")
   expect_type(sumstats$lower, "double")
   expect_type(sumstats$upper, "double")
-  
+
   # Check the dimensions of sumstats
   expect_gt(nrow(sumstats), 0)
   expect_equal(ncol(sumstats), 5)
