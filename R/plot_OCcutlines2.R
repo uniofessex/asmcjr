5e9404dfa7ae1b34ff5853e3c078947617eb4f64
David Liao <davidycliao@gmail.com>
2024-08-15 15:59:18 +0100

    UPDATE

--- R/plot_OCcutlines2.R
+++ R/plot_OCcutlines2.R
@@ -22,38 +22,88 @@
 #' }
 #'
 #' @export
+# plot_OCcutlines2 <- function (x, main.title = "Cutting Lines", d1.title = "First Dimension", 
+#                               d2.title = "Second Dimension", lines = 50, dims = c(1, 2), 
+#                               lwd = 2, ...) 
+# {
+#   if (!class(x) == "OCobject") 
+#     stop("Input is not of class 'OCobject'.")
+#   if (x$dimensions == 1) 
+#     stop("All angles in 1D Optimal Classification are 90 degrees.")
+#   if (length(dims) != 2) 
+#     stop("'dims' must be an integer vector of length 2.")
+#   if(length(lines) == 1){
+#     if (lines < 1) {
+#       stop("'Lines' must be less than 1.")
+#     }
+#   }
+#   cutlineData <- cbind(x$rollcalls[, paste("normVector", dims[1], 
+#                                            "D", sep = "")], x$rollcalls[, paste("normVector", dims[2], 
+#                                                                                 "D", sep = "")], x$rollcalls[, "midpoints"])
+#   cutlineData <- na.omit(cutlineData)
+#   suppressWarnings(symbols(x = 0, y = 0, circles = 1, inches = FALSE, 
+#                            asp = 1, main = main.title, xlab = d1.title, ylab = d2.title, 
+#                            xlim = c(-1, 1), ylim = c(-1, 1), cex.main = 1.2, cex.lab = 1.2, 
+#                            font.main = 2, lwd = 2, fg = "grey", frame.plot = FALSE, 
+#                            ...))
+#   if (length(lines) == 1){
+#     if(lines < dim(cutlineData)[1]) {
+#       cutlineData <- cutlineData[sample(1:dim(cutlineData)[1], lines), ]
+#     }
+#   } 
+#   if(length(lines) > 1){
+#     cutlineData <- cutlineData[lines, ]
+#   }
+#   suppressWarnings(apply(cutlineData, 1, add_OCcutline, lwd = lwd))
+# }  
+# 
+
 plot_OCcutlines2 <- function (x, main.title = "Cutting Lines", d1.title = "First Dimension", 
                               d2.title = "Second Dimension", lines = 50, dims = c(1, 2), 
                               lwd = 2, ...) 
 {
-  if (!class(x) == "OCobject") 
+  # Check if x is of class 'OCobject'
+  if (!inherits(x, "OCobject")) 
     stop("Input is not of class 'OCobject'.")
+  
+  # Check if the dimensions in x is 1
   if (x$dimensions == 1) 
     stop("All angles in 1D Optimal Classification are 90 degrees.")
+  
+  # Check if dims has length of 2
   if (length(dims) != 2) 
     stop("'dims' must be an integer vector of length 2.")
-  if(length(lines) == 1){
-    if (lines < 1) {
-      stop("'Lines' must be less than 1.")
-    }
+  
+  # Validate the 'lines' parameter
+  if (length(lines) == 1 && lines < 1) {
+    stop("'Lines' must be greater than 1.")
   }
+  
+  # Prepare cutline data
   cutlineData <- cbind(x$rollcalls[, paste("normVector", dims[1], 
-                                           "D", sep = "")], x$rollcalls[, paste("normVector", dims[2], 
-                                                                                "D", sep = "")], x$rollcalls[, "midpoints"])
+                                           "D", sep = "")], 
+                       x$rollcalls[, paste("normVector", dims[2], 
+                                           "D", sep = "")], 
+                       x$rollcalls[, "midpoints"])
+  
+  # Remove NA values
   cutlineData <- na.omit(cutlineData)
+  
+  # Draw the circle and setup plot
   suppressWarnings(symbols(x = 0, y = 0, circles = 1, inches = FALSE, 
                            asp = 1, main = main.title, xlab = d1.title, ylab = d2.title, 
                            xlim = c(-1, 1), ylim = c(-1, 1), cex.main = 1.2, cex.lab = 1.2, 
                            font.main = 2, lwd = 2, fg = "grey", frame.plot = FALSE, 
                            ...))
-  if (length(lines) == 1){
-    if(lines < dim(cutlineData)[1]) {
-      cutlineData <- cutlineData[sample(1:dim(cutlineData)[1], lines), ]
-    }
-  } 
-  if(length(lines) > 1){
+  
+  # Subsample or select lines for plotting
+  if (length(lines) == 1 && lines < nrow(cutlineData)) {
+    cutlineData <- cutlineData[sample(1:nrow(cutlineData), lines), ]
+  } else if (length(lines) > 1) {
     cutlineData <- cutlineData[lines, ]
   }
+  
+  # Apply the add_OCcutline function to each row of cutlineData
   suppressWarnings(apply(cutlineData, 1, add_OCcutline, lwd = lwd))
 }  
 
